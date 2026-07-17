cfc50c9a417d51c26078ac252521865177a81605
tbcbalakrishnayerragunta-ai <tbcbalakrishnayerragunta@gmail.com>
2026-07-17 17:59:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/south-indian-food/',
+  base: '/', // 👈 FIXED
 
   plugins: [
     react(),
